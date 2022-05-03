98763580785d5ffd28e4746df90d8cde1a64d850
Bohdan Bryk <bohdan.bryk@ucu.edu.ua>
2022-05-03 23:55:05 +0300

    add file

---
+++ homework2.js
@@ -0,0 +1,42 @@
+/* DONT CHANGE THIS CODE - START */
+function wait(ms = 1000) { return new Promise(resolve => setTimeout(resolve, ms)) }
+
+class Dish {
+    constructor(cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    async cook() {
+        const actualCookingTime = this.cookingTime * (1 + Math.random()) * 100;
+        await wait(actualCookingTime);
+        return this;
+    }
+}
+/* DONT CHANGE THIS CODE - END */
+
+/*
+    YOUR CODE HERE
+*/
+
+async function test() {
+    const kitchen = new Kitchen();
+    kitchen.addToFridge([
+        new Ingridient('potato', 1),
+        new Ingridient('spaghetti', 1),
+        new Ingridient('meat', 3),
+        new Ingridient('tomato', 2)
+    ])
+
+    kitchen.order(new Bolognese()); // Bolognese extends Dish (cookingTime = 10)
+    kitchen.order(new MashedPotatoes()); // MashedPotatoes extends Dish (cookingTime = 8)
+    kitchen.order(new Steak()); // Steak extends Dish (cookingTime = 7)
+
+    // Feel free to experiment with various dishes and ingridients
+
+    await kitchen.cookFastestOrder(); // Returns fastest dish to make
+    await kitchen.cookAllOrders(); // Returns two dishes in array
+
+    kitchen.order(new SteakAndFries()); // Throws Error: Not enough ingridients in fridge
+}
+
+test();
