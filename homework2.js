bbe07a771520e8ad07e94742d7cc5d642df1d6a4
Bohdan Bryk <bohdan.bryk@ucu.edu.ua>
2022-05-04 13:15:05 +0300

    last update file

--- homework2.js
+++ homework2.js
@@ -70,7 +70,6 @@ class Kitchen{
         var arrayLength = this.fridge.length;
 
         for (var i = 0; i < arrayLength; i++) {
-            // console.log(this.fridge[i]);
             if (this.fridge[i].name === ingridient.name ){
                 this.fridge[i].value -= ingridient.value
                 if (this.fridge[i].value < 0){
@@ -83,6 +82,34 @@ class Kitchen{
         throw new Error("Not enough ingredients in fridge");
 
     }
+
+    cookFastestOrder(){
+        let fastestOrder = this.orders[0]
+        let curInd = 0
+        var arrayLength = this.orders.length;
+
+        for (var i = 1; i < arrayLength; i++) {
+            if (fastestOrder.cookingTime > this.orders[i].cookingTime){
+                fastestOrder = this.orders[i]
+                curInd = i
+            }
+        }
+        this.orders.splice(curInd, 1);
+        return fastestOrder.cook();
+
+    }
+ 
+    async cookAllOrders() {
+        var arrayLength = this.orders.length;
+
+        for (var i = 1; i < arrayLength; i++) {
+            await this.orders[i].cook()
+        }
+        
+        let finishedOrders = this.orders;
+        this.orderqueue = [];
+        return finishedOrders;
+    }
 }
 
 
@@ -90,6 +117,7 @@ class Bolognese extends Dish{
     constructor(){
         super(10);
         this.ingridients = [new Ingridient("meat",1), new Ingridient("spaghetti",1)]
+        this.name = "Bolognese"
     }
 }
 
@@ -97,20 +125,26 @@ class MashedPotatoes extends Dish{
     constructor(){
         super(8);
         this.ingridients = [new Ingridient("potato",1)]
+        this.name = "MashedPotatoes"
+
     }
 }
 
 class Steak extends Dish{
     constructor(){
-        super(8);
+        super(7);
         this.ingridients = [new Ingridient("meat",1)]
+        this.name = "Steak"
+
     }
 }
 
 class SteakAndFries extends Dish{
     constructor(){
-        super(8);
+        super(12);
         this.ingridients = [new Ingridient("meat",1), new Ingridient("potato", 2)]
+        this.name = "SteakAndFries"
+
     }
 }
 
@@ -124,30 +158,30 @@ async function test() {
         new Ingridient('meat', 3),
         new Ingridient('tomato', 2)
     ])
-    // console.log(kitchen.fridge);
-    for (let ingredient of kitchen.fridge) {
-        console.log(ingredient)
-    }
-    kitchen.order(new Bolognese()); // Bolognese extends Dish (cookingTime = 10)
-    kitchen.order(new Bolognese()); // Bolognese extends Dish (cookingTime = 10)
-    console.log("start")
-    for (let ingredient of kitchen.fridge) {
-        console.log(ingredient)
-    }
-    console.log("end")
+    let curDish = new Bolognese()
+    kitchen.order(curDish); // Bolognese extends Dish (cookingTime = 10)
+    console.log(curDish.name+" ordered ")
 
-    // kitchen.order(new MashedPotatoes()); // MashedPotatoes extends Dish (cookingTime = 8)
-    // kitchen.order(new Steak()); // Steak extends Dish (cookingTime = 7)
+    curDish = new MashedPotatoes()
+    kitchen.order(curDish); // MashedPotatoes extends Dish (cookingTime = 8)
+    console.log(curDish.name+" ordered ")
+
+    curDish = new Steak()
+    kitchen.order(curDish); // Steak extends Dish (cookingTime = 7)
+    console.log(curDish.name+" ordered ")
+
 
     // // Feel free to experiment with various dishes and ingridients
 
-    // // await kitchen.cookFastestOrder(); // Returns fastest dish to make
-    // // await kitchen.cookAllOrders(); // Returns two dishes in array
+    let fastest =await kitchen.cookFastestOrder(); // Returns fastest dish to make
+    console.log("Cooked the fastest order: " + fastest.name + " in " + fastest.cookingTime + " minutes")
+    let allCooked = await kitchen.cookAllOrders(); // Returns two dishes in array
+    for (var i = 0; i < allCooked.length; i++) {
+        console.log("Cooked the order: " + allCooked[i].name + " in " + allCooked[i].cookingTime + " minutes")
+    }
 
-    // kitchen.order(new SteakAndFries()); // Throws Error: Not enough ingridients in fridge
+    kitchen.order(new SteakAndFries()); // Throws Error: Not enough ingridients in fridge
 }
-console.log(10);
-test();
-// throw 'Not enough ingridients in fridge'
 
-console.log(20);
+test();
+
